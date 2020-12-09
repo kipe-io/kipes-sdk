3e43babb67558ed5fc395296d415c8088bb7a67c
Jens Günther <jens.guenther@age-works.de>
2020-12-09 11:36:36 +0100

    changed ksql table impulse_trading_screen_latest

---
+++ stage.backtest/src/main/java/de/tradingpulse/stage/backtest/BacktestStageConstants.java
@@ -0,0 +1,9 @@
+package de.tradingpulse.stage.backtest;
+
+public final class BacktestStageConstants {
+	
+	public static final String STAGE_NAME = "stg-backtest";
+	
+	private BacktestStageConstants() {}
+
+}
