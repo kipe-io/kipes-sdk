22786330284f43335dcfa60faa85d3a6a302145c
John Gerassimou <john.gerassimou@gmail.com>
2023-03-22 01:03:06 -0400

    address pr comments

--- streams-kafka/src/main/java/io/kipe/streams/kafka/processors/expressions/stats/Count.java
+++ streams-kafka/src/main/java/io/kipe/streams/kafka/processors/expressions/stats/Count.java
@@ -1,6 +1,6 @@
 /*
- * Kipe Streams Kafka - Kipe Streams SDK
- * Copyright © 2023 Kipe.io
+ * Kipes SDK for Kafka - The High-Level Event Processing SDK.
+ * Copyright © 2023 kipe.io
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -18,37 +18,13 @@
 package io.kipe.streams.kafka.processors.expressions.stats;
 
 import io.kipe.streams.kafka.processors.StatsExpression;
-
-/**
- * Stats expression to count records.
- * <p>
- * This class provides a singleton instance of the Count expression and a default field name "count"
- * which will be used to store the count value in the resulting record.
- * <p>
- * The valueFunction provided in the constructor takes in a key and value and returns the count value
- * by first attempting to retrieve the count value from the provided field name in the value,
- * and if it is not present, it defaults to 1.
- * <p>
- * The class also provides a static factory method count() to retrieve the singleton instance.
- */
 public class Count extends StatsExpression {
 
 	public static final String DEFAULT_FIELD = "count";
 	private static final Count SINGLETON = new Count();
-
-	/**
-	 * Returns the singleton instance of this class
-	 *
-	 * @return Count singleton instance
-	 */
 	public static Count count() {
 		return SINGLETON;
 	}
-
-	/**
-	 * Constructor for Count class, which calls the constructor of the parent class {@link StatsExpression}
-	 * with the default field name "count". It also sets the valueFunction in the constructor.
-	 */
 	private Count() {
 		super(DEFAULT_FIELD);
 		this.valueFunction = (key, value) -> {
