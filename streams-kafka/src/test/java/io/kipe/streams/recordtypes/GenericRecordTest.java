7210aaa7b52f5754562b62b356981f2ae7fa9520
John Gerassimou <john.gerassimou@gmail.com>
2023-03-27 01:18:43 -0400

    add paramterized test for generic record

--- streams-kafka/src/test/java/io/kipe/streams/recordtypes/GenericRecordTest.java
+++ streams-kafka/src/test/java/io/kipe/streams/recordtypes/GenericRecordTest.java
@@ -18,7 +18,13 @@
 package io.kipe.streams.recordtypes;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -28,8 +34,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.IOException;
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Test class for {@link GenericRecord}.
@@ -115,106 +126,137 @@ class GenericRecordTest {
 	// serde
 	// ------------------------------------------------------------------------
 
-	/**
-	 * Test complex object in GenericRecord field.
-	 *
-	 * @throws JsonProcessingException if a problem was encountered when processing.
-	 */
-	@Test
-	void test_complex_object_in_field() throws JsonProcessingException {
-		r.set(FIELD, new TestType(VALUE));
+	private static Stream<Arguments> dataProvider() {
+		return Stream.of(
+				// Basic data types
+				arguments("stringValue", "Hello, Kipes!"),
+				arguments("intValue", 42),
+				arguments("doubleValue", 3.14),
+				arguments("booleanValue", true),
 
-		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writeValueAsString(r);
-		
-		GenericRecord serdeRecord = mapper.readValue(json, GenericRecord.class);
-		assertEquals(r, serdeRecord);
-		assertEquals(VALUE, ((TestType)r.get(FIELD)).getContent());
-	}
+				// Custom data type
+				arguments("customTypeValue", new GenericRecordTest.TestType("customContent")),
 
-	/**
-	 * Test array, set, list, map of simple objects in GenericRecord field.
-	 */
-	@Test
-	void test_collection_of_simple_objects_in_field() throws JsonProcessingException {
-		Set<String> set = new HashSet<>(Arrays.asList("one", "two", "three"));
-		List<String> list = Arrays.asList("one", "two", "three");
-		String[] array = new String[]{"one", "two", "three"};
+				// Collection data types
+				arguments("listValue", List.of("one", "two", "three")),
+				arguments("mapValue", Map.of("one", 1, "two", 2, "three", 3)),
+				arguments("emptyListValue", Collections.EMPTY_LIST),
+				arguments("emptyMapValue", Collections.EMPTY_MAP),
+				arguments("nestedListValue", List.of(List.of("one", "two"), List.of("three", "four"))),
+				arguments("nestedMapValue", Map.of("first", Map.of("one", 1, "two", 2), "second", Map.of("three", 3, "four", 4))),
+				arguments("stringListValue", List.of("one", "two", "three")),
+				arguments("intMapValue", Map.of("one", 1, "two", 2, "three", 3)),
+				arguments("setOfMapsValue", Set.of(Map.of("one", 1), Map.of("two", 2))),
+				arguments("listOfSetsValue", List.of(Set.of("one", "two"), Set.of("three", "four"))),
+				arguments("setOfSetsValue", Set.of(Set.of("one", "two"), Set.of("three", "four"))),
+				arguments("mapOfListsValue", Map.of("first", List.of("one", "two"), "second", List.of("three", "four"))),
 
-		Map<String, Integer> map = new HashMap<>();
-		map.put("one", 1);
-		map.put("two", 2);
-		map.put("three", 3);
+				// Array data types
+				arguments("intArrayValue", new Integer[]{1, 2, 3}),
+				arguments("doubleArrayValue", new Double[]{1.0, 2.0, 3.0}),
+				arguments("booleanArrayValue", new Boolean[]{true, false, true}),
+				arguments("stringArrayValue", new String[]{"hello", "world"}),
+				arguments("objectArrayValue", new Object[]{"hello", 42, true}),
+				arguments("intArray2DValue", new Integer[][]{{1, 2}, {3, 4}}),
+				arguments("objectArray3DValue", new Object[][][]{{{"one", "two"}, {"three", "four"}}, {{"five", "six"}, {"seven", "eight"}}}),
 
-		r.set("listField", list);
-		r.set("setField", set);
-		r.set("arrayField", array);
-		r.set("mapField", map);
+				// Mixed data types
+				arguments("genericListValue", List.of("hello", 42, true)),
+				arguments("genericMapValue", Map.of("one", "hello", "two", 42, "three", true)),
+				arguments("nestedGenericListValue", List.of(List.of("one", 1), List.of(true, 3.14))),
+				arguments("customParameterizedValue", new GenericRecordTest4.CustomParameterizedType<>("customParameterizedContent", Map.of("one", 1, "two", 2))),
+				arguments("customNestedParameterizedValue", new GenericRecordTest4.CustomNestedParameterizedType<>(List.of(Map.of("one", 1, "two", 2), Map.of("three", 3, "four", 4)), Set.of("hello", "world"))),
 
-		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writeValueAsString(r);
+				// Empty record
+				arguments("emptyRecordValue", new GenericRecord()),
 
-		GenericRecord serdeRecord = mapper.readValue(json, GenericRecord.class);
-
-		assertEquals(list, serdeRecord.get("listField"));
-		assertEquals(set, serdeRecord.get("setField"));
-		assertArrayEquals(array, serdeRecord.get("arrayField"));
-		assertEquals(map, serdeRecord.get("mapField"));
-	}
-
-	/**
-	 * Test collection of complex objects in GenericRecord field.
-	 */
-	@Test
-	void test_collection_of_complex_objects_in_field() throws JsonProcessingException {
-		List<TestType> complexObjects = Arrays.asList(
-				new TestType("first"),
-				new TestType("second"),
-				new TestType("third")
+				// Null
+				arguments("nullValue", null)
 		);
+	}
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    void test_values_in_generic_record(String fieldName, Object fieldValue) throws JsonProcessingException {
+        r.set(fieldName, fieldValue);
 
-		r.set(FIELD, complexObjects);
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(r);
+
+        GenericRecord serdeRecord = mapper.readValue(json, GenericRecord.class);
+
+        // This temporary approach demonstrates field equality as we cannot always compare GenericRecords directly.
+        // Unordered sets makes this challenging and requires workarounds.
+
+        if (fieldValue == null) {
+            assertEquals(r, serdeRecord);
+        } else {
+            if (fieldValue.getClass().isArray()) {
+                assertArrayEquals(r.get(fieldName), (Object[]) serdeRecord.get(fieldName));
+            } else if (fieldValue instanceof Set) {
+                Object sortedRFieldValue = sortNestedCollections(r.get(fieldName));
+                Object sortedSerdeRecordFieldValue = sortNestedCollections(serdeRecord.get(fieldName));
+                assertIterableEquals(Collections.singleton(sortedRFieldValue), Collections.singleton(sortedSerdeRecordFieldValue));
+            } else if (fieldValue instanceof Collection) {
+                assertIterableEquals(Collections.singleton(r.get(fieldName)), Collections.singleton(serdeRecord.get(fieldName)));
+            } else {
+                assertEquals(r, serdeRecord);
+            }
+        }
+    }
+
+    private Object sortNestedCollections(Object collection) {
+        if (collection instanceof List) {
+            List<?> list = (List<?>) collection;
+            List<Object> sortedList = new ArrayList<>(list);
+            sortedList.sort(Comparator.comparing(Object::toString));
+            sortedList.replaceAll(this::sortNestedCollections);
+            return sortedList;
+        } else if (collection instanceof Set) {
+            Set<?> set = (Set<?>) collection;
+            List<Object> sortedList = new ArrayList<>(set);
+            sortedList.sort(Comparator.comparing(Object::toString));
+            sortedList.replaceAll(this::sortNestedCollections);
+            return new LinkedHashSet<>(sortedList);
+        } else if (collection instanceof Map) {
+            Map<?, ?> map = (Map<?, ?>) collection;
+            Map<Object, Object> sortedMap = new LinkedHashMap<>();
+            map.entrySet().stream()
+                    .sorted(Map.Entry.comparingByKey(Comparator.comparing(Object::toString)))
+                    .forEach(entry -> sortedMap.put(entry.getKey(), sortNestedCollections(entry.getValue())));
+            return sortedMap;
+        }
+        return collection;
+    }
+
+	@Test
+	void testListWithCyclicReferencesValue() {
+		List<Object> listWithCyclicReferences = new ArrayList<>();
+		listWithCyclicReferences.add("one");
+		listWithCyclicReferences.add(listWithCyclicReferences);
+
+		r.set("listWithCyclicReferencesValue", listWithCyclicReferences);
 
 		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writeValueAsString(r);
 
-		GenericRecord serdeRecord = mapper.readValue(json, GenericRecord.class);
-
-		List<Map<String, String>> deserializedTypes = serdeRecord.get("field");
-
-		for (int i = 0; i < complexObjects.size(); i++) {
-			assertEquals(complexObjects.get(i).getContent(), deserializedTypes.get(i).get("content"));
-		}
-
+		assertThrows(JsonProcessingException.class, () -> {
+			String json = mapper.writeValueAsString(r);
+		});
 	}
 
-	/**
-	 * Test collection of collections in GenericRecord field.
-	 */
+	// Note: The Jdk8Module is required to properly handle Optional fields in a GenericRecord with the ObjectMapper.
+	// However, at the moment, Kipes does not support this module out of the box.
 	@Test
-	void test_collection_of_collections_in_field() throws JsonProcessingException {
-		List<String> innerList1 = Arrays.asList("one", "two");
-		List<String> innerList2 = Arrays.asList("three", "four");
-		List<String> innerList3 = Arrays.asList("five", "six");
-
-		List<List<String>> listOfLists = new ArrayList<>();
-		listOfLists.add(innerList1);
-		listOfLists.add(innerList2);
-		listOfLists.add(innerList3);
-
-		r.set("listOfListsField", listOfLists);
+	void testListWithOptionalValue() throws JsonProcessingException {
+		Optional<String> optionalValue = Optional.of("value");
+		r.set("optionalValue", optionalValue);
 
 		ObjectMapper mapper = new ObjectMapper();
+
 		String json = mapper.writeValueAsString(r);
 
-		GenericRecord serdeRecord = mapper.readValue(json, GenericRecord.class);
-		assertEquals(r, serdeRecord);
-
-		List<List<String>> deserializedListOfLists = serdeRecord.get("listOfListsField");
-		assertEquals(listOfLists.size(), deserializedListOfLists.size());
-		for (int i = 0; i < listOfLists.size(); i++) {
-			assertEquals(listOfLists.get(i), deserializedListOfLists.get(i));
-		}
+		assertThrows(UnrecognizedPropertyException.class, () -> {
+			GenericRecord serdeRecord = mapper.readValue(json, GenericRecord.class);
+		});
 	}
 	
 	// ------------------------------------------------------------------------
@@ -230,5 +272,53 @@ class GenericRecordTest {
 	public static class TestType {
 		
 		private String content;
+	}
+
+	@Test
+	void testNonSerializableCustomType() {
+		GenericRecordTest4.NonSerializableCustomType customType = new GenericRecordTest4.NonSerializableCustomType("customData");
+		r.set("nonSerializableCustomType", customType);
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		assertThrows(JsonProcessingException.class, () -> {
+			String json = mapper.writeValueAsString(r);
+		});
+	}
+
+	@JsonSerialize(using = NonSerializableCustomType.CustomSerializer.class)
+	public static class NonSerializableCustomType {
+		private final String customData;
+
+		public NonSerializableCustomType(String customData) {
+			this.customData = customData;
+		}
+
+		public String getCustomData() {
+			return customData;
+		}
+
+		public static class CustomSerializer extends JsonSerializer<NonSerializableCustomType> {
+			@Override
+			public void serialize(NonSerializableCustomType value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+				throw new IOException("Cannot serialize NonSerializableCustomType");
+			}
+		}
+	}
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class CustomParameterizedType<T, U> {
+		private T content1;
+		private U content2;
+	}
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class CustomNestedParameterizedType<T, U> {
+		private List<T> listContent;
+		private Set<U> setContent;
 	}
 }
