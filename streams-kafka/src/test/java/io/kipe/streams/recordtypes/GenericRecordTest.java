17d0fa74e9a6f17faf293a9495f4ae80604cd50b
John Gerassimou <john.gerassimou@gmail.com>
2023-03-26 23:19:07 -0400

    add more test cases for generic record

--- streams-kafka/src/test/java/io/kipe/streams/recordtypes/GenericRecordTest.java
+++ streams-kafka/src/test/java/io/kipe/streams/recordtypes/GenericRecordTest.java
@@ -25,24 +25,11 @@ import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.kipe.streams.recordtypes.GenericRecord;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
-///**
-// * Test class for {@link GenericRecord}. This class includes test cases for the following functionality:
-// * <ul>
-// * <li>{@link GenericRecord#withNewFieldsFrom(GenericRecord)}</li>
-// * <p>
-// * <li>{@link GenericRecord#set(String, Object)} and {@link GenericRecord#get(String)}</li>
-// * <p>
-// * <li>{@link GenericRecord#remove(String)}</li>
-// * <p>
-// * <li>Serialization and Deserialization (serde) using Jackson</li>
-// *
-// * </ul>
+import java.util.*;
 
 /**
  * Test class for {@link GenericRecord}.
@@ -129,12 +116,12 @@ class GenericRecordTest {
 	// ------------------------------------------------------------------------
 
 	/**
-	 * Test {@link GenericRecord} is serdeable.
+	 * Test complex object in GenericRecord field.
 	 *
 	 * @throws JsonProcessingException if a problem was encountered when processing.
 	 */
 	@Test
-	void test_is_serdeable() throws JsonProcessingException {
+	void test_complex_object_in_field() throws JsonProcessingException {
 		r.set(FIELD, new TestType(VALUE));
 
 		ObjectMapper mapper = new ObjectMapper();
@@ -143,6 +130,91 @@ class GenericRecordTest {
 		GenericRecord serdeRecord = mapper.readValue(json, GenericRecord.class);
 		assertEquals(r, serdeRecord);
 		assertEquals(VALUE, ((TestType)r.get(FIELD)).getContent());
+	}
+
+	/**
+	 * Test array, set, list, map of simple objects in GenericRecord field.
+	 */
+	@Test
+	void test_collection_of_simple_objects_in_field() throws JsonProcessingException {
+		Set<String> set = new HashSet<>(Arrays.asList("one", "two", "three"));
+		List<String> list = Arrays.asList("one", "two", "three");
+		String[] array = new String[]{"one", "two", "three"};
+
+		Map<String, Integer> map = new HashMap<>();
+		map.put("one", 1);
+		map.put("two", 2);
+		map.put("three", 3);
+
+		r.set("listField", list);
+		r.set("setField", set);
+		r.set("arrayField", array);
+		r.set("mapField", map);
+
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(r);
+
+		GenericRecord serdeRecord = mapper.readValue(json, GenericRecord.class);
+
+		assertEquals(list, serdeRecord.get("listField"));
+		assertEquals(set, serdeRecord.get("setField"));
+		assertArrayEquals(array, serdeRecord.get("arrayField"));
+		assertEquals(map, serdeRecord.get("mapField"));
+	}
+
+	/**
+	 * Test collection of complex objects in GenericRecord field.
+	 */
+	@Test
+	void test_collection_of_complex_objects_in_field() throws JsonProcessingException {
+		List<TestType> complexObjects = Arrays.asList(
+				new TestType("first"),
+				new TestType("second"),
+				new TestType("third")
+		);
+
+		r.set(FIELD, complexObjects);
+
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(r);
+
+		GenericRecord serdeRecord = mapper.readValue(json, GenericRecord.class);
+
+		List<Map<String, String>> deserializedTypes = serdeRecord.get("field");
+
+		for (int i = 0; i < complexObjects.size(); i++) {
+			assertEquals(complexObjects.get(i).getContent(), deserializedTypes.get(i).get("content"));
+		}
+
+	}
+
+	/**
+	 * Test collection of collections in GenericRecord field.
+	 */
+	@Test
+	void test_collection_of_collections_in_field() throws JsonProcessingException {
+		List<String> innerList1 = Arrays.asList("one", "two");
+		List<String> innerList2 = Arrays.asList("three", "four");
+		List<String> innerList3 = Arrays.asList("five", "six");
+
+		List<List<String>> listOfLists = new ArrayList<>();
+		listOfLists.add(innerList1);
+		listOfLists.add(innerList2);
+		listOfLists.add(innerList3);
+
+		r.set("listOfListsField", listOfLists);
+
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(r);
+
+		GenericRecord serdeRecord = mapper.readValue(json, GenericRecord.class);
+		assertEquals(r, serdeRecord);
+
+		List<List<String>> deserializedListOfLists = serdeRecord.get("listOfListsField");
+		assertEquals(listOfLists.size(), deserializedListOfLists.size());
+		for (int i = 0; i < listOfLists.size(); i++) {
+			assertEquals(listOfLists.get(i), deserializedListOfLists.get(i));
+		}
 	}
 	
 	// ------------------------------------------------------------------------
