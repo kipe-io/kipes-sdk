1757596fe8d145074eb6666136dad8ad94784507
John Gerassimou <john.gerassimou@gmail.com>
2023-04-02 23:47:05 -0400

    stats: optimize median expression using 2-heap approach

--- streams-kafka/src/main/java/io/kipe/streams/kafka/processors/expressions/stats/Median.java
+++ streams-kafka/src/main/java/io/kipe/streams/kafka/processors/expressions/stats/Median.java
@@ -1,27 +1,9 @@
-/*
- * Kipes SDK for Kafka - The High-Level Event Processing SDK.
- * Copyright © 2023 kipe.io
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 package io.kipe.streams.kafka.processors.expressions.stats;
 
 import io.kipe.streams.kafka.processors.StatsExpression;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * The Median class calculates the median value of a data stream for a specified field.
@@ -41,30 +23,47 @@ public class Median extends StatsExpression {
 
     /**
      * Initializes the statsFunction to calculate the median by collecting the values for the specified field and
-     * finding the middle value when sorted.
+     * finding the middle value using two heaps.
      */
     private Median(String fieldNameToMedian) {
         super(DEFAULT_FIELD);
         this.statsFunction = (groupKey, value, aggregate) -> {
-            String fieldNameValues = createInternalFieldName("values");
+            String fieldNameLowerHalf = createInternalFieldName("lowerHalf");
+            String fieldNameUpperHalf = createInternalFieldName("upperHalf");
 
-            List<Number> values = aggregate.get(fieldNameValues);
-            if (values == null) {
-                values = new ArrayList<>();
+            PriorityQueue<Double> lowerHalf = aggregate.get(fieldNameLowerHalf);
+            PriorityQueue<Double> upperHalf = aggregate.get(fieldNameUpperHalf);
+
+            if (lowerHalf == null) {
+                lowerHalf = new PriorityQueue<>(Comparator.reverseOrder());
+            }
+            if (upperHalf == null) {
+                upperHalf = new PriorityQueue<>(Comparator.naturalOrder());
             }
 
-            values.add(value.getNumber(fieldNameToMedian));
-            Collections.sort(values, (a, b) -> Double.compare(a.doubleValue(), b.doubleValue()));
+            Double fieldValue = value.getNumber(fieldNameToMedian).doubleValue();
 
-            aggregate.set(fieldNameValues, values);
-
-            int size = values.size();
-            if (size == 0) {
-                return 0.0;
-            } else if (size % 2 == 0) {
-                return (values.get(size / 2 - 1).doubleValue() + values.get(size / 2).doubleValue()) / 2;
+            if (lowerHalf.isEmpty() || fieldValue < lowerHalf.peek()) {
+                lowerHalf.add(fieldValue);
             } else {
-                return values.get(size / 2).doubleValue();
+                upperHalf.add(fieldValue);
+            }
+
+            // Rebalance the heaps
+            while (lowerHalf.size() > upperHalf.size() + 1) {
+                upperHalf.add(lowerHalf.poll());
+            }
+            while (upperHalf.size() > lowerHalf.size()) {
+                lowerHalf.add(upperHalf.poll());
+            }
+
+            aggregate.set(fieldNameLowerHalf, lowerHalf);
+            aggregate.set(fieldNameUpperHalf, upperHalf);
+
+            if (lowerHalf.size() == upperHalf.size()) {
+                return (lowerHalf.peek() + upperHalf.peek()) / 2;
+            } else {
+                return lowerHalf.peek();
             }
         };
     }
