bf15c9daea9cb2f9baebdc530bdd73adc541eb4e
John Gerassimou <john.gerassimou@gmail.com>
2023-04-03 23:52:28 -0400

    stats: add variance expression

---
+++ streams-kafka/src/main/java/io/kipe/streams/kafka/processors/expressions/stats/Variance.java
@@ -0,0 +1,94 @@
+/*
+ * Kipes SDK for Kafka - The High-Level Event Processing SDK.
+ * Copyright © 2023 kipe.io
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.kipe.streams.kafka.processors.expressions.stats;
+
+import io.kipe.streams.kafka.processors.StatsExpression;
+
+/**
+ * The Variance class calculates the variance of values in a data stream using Welford's algorithm for better numerical
+ * stability.
+ */
+public class Variance extends StatsExpression {
+    public static final String DEFAULT_SAMPLE_VARIANCE_FIELD = "var";
+    public static final String DEFAULT_POPULATION_VARIANCE_FIELD = "varP";
+
+
+    public enum VarianceType {
+        SAMPLE, POPULATION
+    }
+
+    /**
+     * Returns a new Variance instance for the specified field and variance type.
+     *
+     * @param fieldNameToVariance the field for which the variance will be calculated
+     * @param varianceType        the type of variance to calculate (sample or population)
+     * @return a new Variance instance for the given field
+     */
+    public static Variance var(String fieldNameToVariance, VarianceType varianceType) {
+        String defaultField = varianceType == VarianceType.SAMPLE ? DEFAULT_SAMPLE_VARIANCE_FIELD : DEFAULT_POPULATION_VARIANCE_FIELD;
+        return new Variance(fieldNameToVariance, varianceType, defaultField);
+    }
+
+    /**
+     * Returns a new Variance instance for the specified field, calculating sample variance.
+     *
+     * @param fieldNameToVariance the field for which the variance will be calculated
+     * @return a new Variance instance for the given field
+     */
+    public static Variance var(String fieldNameToVariance) {
+        return var(fieldNameToVariance, VarianceType.SAMPLE);
+    }
+
+    /**
+     * Returns a new Variance instance for the specified field, calculating population variance.
+     *
+     * @param fieldNameToVariance the field for which the variance will be calculated
+     * @return a new Variance instance for the given field
+     */
+    public static Variance varp(String fieldNameToVariance) {
+        return var(fieldNameToVariance, VarianceType.POPULATION);
+    }
+
+    /**
+     * Initializes the statsFunction to calculate the variance for the specified field.
+     */
+    private Variance(String fieldNameToVariance, VarianceType varianceType, String defaultField) {
+        super(defaultField);
+        this.statsFunction = (groupKey, value, aggregate) -> {
+            String fieldNameCount = createInternalFieldName("count");
+            String fieldNameMean = createInternalFieldName("mean");
+            String fieldNameSsd = createInternalFieldName("ssd");
+
+            double oldMean = aggregate.getDouble(fieldNameMean) == null ? 0.0 : aggregate.getDouble(fieldNameMean);
+            double count = aggregate.getNumber(fieldNameCount) == null ? 0.0 : aggregate.getNumber(fieldNameCount).doubleValue();
+            double fieldValue = value.getDouble(fieldNameToVariance);
+            count += 1;
+            double newMean = oldMean + (fieldValue - oldMean) / count;
+            double oldS = aggregate.getDouble(fieldNameSsd) == null ? 0.0 : aggregate.getDouble(fieldNameSsd);
+            double newS = oldS + (fieldValue - oldMean) * (fieldValue - newMean);
+
+            aggregate.set(fieldNameCount, count);
+            aggregate.set(fieldNameMean, newMean);
+            aggregate.set(fieldNameSsd, newS);
+
+            double variance = newS / (varianceType.equals(VarianceType.POPULATION) ? count : count - 1);
+            return count <= 1 ? 0.0 : variance;
+        };
+    }
+}
+g
